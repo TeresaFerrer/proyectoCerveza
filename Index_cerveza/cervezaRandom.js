3ccdee8d6f6e91c256c82d32e85c1aafa6f8a651
TeresaFerrer <66690006+TeresaFerrer@users.noreply.github.com>
2021-06-14 02:59:49 +0200

    Add files via upload

---
+++ Index_cerveza/cervezaRandom.js
@@ -0,0 +1,27 @@
+const urlParams = new URLSearchParams(window.location.search);
+const beers = urlParams.get("beers");
+
+fetch("https://api.punkapi.com/v2/beers/random")
+  .then(function (res){
+    return res.json();
+  })
+  .then(function (beer) {
+
+    const node = document.getElementById("detail");
+    beer.forEach(beers=> {
+
+      node.innerHTML += 
+      `<p>${beers.name}</p>
+      <img src="${beers.image_url}"/>
+      <p>${beers.id}</p>`;
+
+      console.log(beers)})
+  });
+
+  
+  function getID(param) {
+    const results = param.split("/");
+    return results[results.length - 2];
+  }
+  
+  
